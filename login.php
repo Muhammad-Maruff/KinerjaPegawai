7df889609c7cfc25c81113348c2cadab36865161
Maruf <99467481+Muhammad-Maruff@users.noreply.github.com>
2022-10-14 09:08:05 +0700

    commit pertama

---
+++ login.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <title>LOGIN | ADMIN</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<!-- Image and text -->
+<nav class="navbar">
+  <a class="navbar-brand" href="#">
+    <img src="https://www.patinews.com/wp-content/uploads/2015/03/logo-pln-pati.jpg" width="30" height="30" class="d-inline-block align-top foto" alt="" >
+    <p class="judul">Sistem Manajemen Kinerja Pegawai (SIMKP)</p> 
+  </a>
+</nav>   
+
+<h1>
+    <img src="image/LOGO_PLN.png" alt="" class="logo">
+</h1>
+<div class="bg-login">
+<div class="box-login">
+    <h2 class="jd-admin">ADMIN</h2>
+    <form action="" method="POST">
+        <section id="label">
+        <label for="">USERNAME</label>
+        <input type="text" name="user" placeholder="Username..." class="input-control" id="input-user">
+        </section>
+        <label for="">PASSWORD</label>
+        <input type="password" name="pass" placeholder="Password..." class="input-control" id="input-pass">
+        <input type="submit" name="submit" value="Login" class="btn-login">
+    </form>
+    </div>
+    </div>
+<script type="text/javascript" src="js/bootstrap.min.css"></script>
+</body>
+</html>
