8e64b8756099cc9a5186052ca69e5654760de4cc
Maruf <99467481+Muhammad-Maruff@users.noreply.github.com>
2022-10-14 10:36:58 +0700

    Menyelesaikan fitur login admin & user

--- login-user.php
+++ login-user.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-    <title>LOGIN | ADMIN</title>
+    <title>LOGIN | USER</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
@@ -22,7 +22,7 @@
 </h1>
 <div class="bg-login">
 <div class="box-login">
-    <h2 class="jd-admin">ADMIN</h2>
+    <h2 class="jd-admin">USER</h2>
     <form action="" method="POST">
         <section id="label">
         <label for="">USERNAME</label>
@@ -40,7 +40,7 @@
         $user = $_POST['user'];
         $pass = $_POST['pass'];
 
-        $cek = mysqli_query($conn, "SELECT * FROM tb_admin WHERE username = '".$user."' AND password = '".$pass."'");
+        $cek = mysqli_query($conn, "SELECT * FROM tb_user WHERE username = '".$user."' AND password = '".$pass."'");
         if(mysqli_num_rows($cek) > 0){
             $d = mysqli_fetch_object($cek);
             $_SESSION['status_login'] = true;
